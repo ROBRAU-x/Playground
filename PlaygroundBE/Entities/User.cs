1c138ecf7c0902baa9ded94d19b2229325df4678
Robin <robin.rauseus@softronic.se>
2021-02-11 10:59:08 +0100

    Added user

---
+++ PlaygroundBE/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlaygroundBE.Entities
+{
+    public class User
+    {
+        private int id { get; set; }
+
+        private int userName { get; set; }
+
+        private int passWord { get; set; }
+    }
+}
